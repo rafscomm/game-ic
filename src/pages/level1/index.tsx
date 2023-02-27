932e086f9407f6dca86b9b024ee5e51d15e33e25
rafscomm <rafaelpereira2033@gmail.com>
2023-02-27 12:25:14 -0300

    fix: bug no mapa

--- src/pages/level1/index.tsx
+++ src/pages/level1/index.tsx
@@ -1,25 +1,34 @@
-import { Marker, Popup, TileLayer, MapContainer } from "react-leaflet";
+import { Marker, TileLayer, MapContainer, Circle } from "react-leaflet";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 import { Principal } from "../selectLevel/styles";
+import { QuestionBox } from "../../components/questionsBox";
+import { PinRed } from "./styles";
 
 export const LevelOne = () => {
+  const LeafIcon = L.Icon.extend({
+    options: {},
+  });
   return (
-    <Principal>
-      <MapContainer
-        center={[51.505, -0.09]}
-        zoom={13}
-        scrollWheelZoom={false}
-        style={{ height: "100%" }}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
-    </Principal>
+    <>
+      <Principal>
+        <QuestionBox
+          questao="Tendo como referência o ponto azul, qual das marcações esta a mais leste dele?"
+          certo="opt3"
+        ></QuestionBox>
+        <MapContainer
+          center={[0, 0]}
+          zoom={3}
+          style={{ height: "100%", width: "100%" }}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[0, 0]}></Marker>
+          <Circle center={[51, 51]} radius={400}></Circle>
+        </MapContainer>
+      </Principal>
+    </>
   );
 };
---
+++ src/pages/level1/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const PinRed = styled.div`
+
+  position: absolute;
+  top: 40%;
+  left: 50%;
+  margin-left: 115px;
+
+  border-radius: 50%;
+  border: 8px solid #fff;
+  width: 8px;
+  height: 8px;
+`
